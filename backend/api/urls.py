3f5f90bf5573cea4a17fdfadd84597b08db3836d
Gabriella <esturrarigabi@gmail.com>
2026-02-05 11:27:16 -0300

    back e front

---
+++ backend/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import listar_usuarios, UsuarioView, UsuarioDetailView, ImovelView, ImovelDetailView, ContratoView, ContratoDetailView, PagamentoView, PagamentoDetailView
+
+urlpatterns = [
+    path('usuarios', listar_usuarios),
+    path('users', UsuarioView.as_view()),
+    path('usuario/<int:pk>', UsuarioDetailView.as_view()),
+
+    path('Imoveis', ImovelView.as_view()),
+    path('Imovel/<int:pk>', ImovelDetailView.as_view()),
+
+    path('Contratos', ContratoView.as_view()),
+    path('Contrato/<int:pk>', ContratoDetailView.as_view()),
+
+    path('Pagamentos', PagamentoView.as_view()),
+    path('Pagamento/<int:pk>', PagamentoDetailView.as_view())
+
+]
